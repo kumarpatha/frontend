05b97c416e54246ba19784dae669bacd21d866e1
atul0514 <61494033+atul0514@users.noreply.github.com>
2021-03-25 09:15:28 +0530

    ch25

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, ViewChild, OnDestroy} from '@angular/core';
-import { first } from 'rxjs/operators';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ViewChild, OnDestroy } from "@angular/core";
+import { first } from "rxjs/operators";
+import { Router, ActivatedRoute } from "@angular/router";
 
-import { User } from '@app/_models';
-import { UserService, AuthenticationService } from '@app/_services';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { environment } from '@environments/environment';
-import { DataTableDirective } from 'angular-datatables';
-import Swal from 'sweetalert2';
+import { User } from "@app/_models";
+import { UserService, AuthenticationService } from "@app/_services";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { environment } from "@environments/environment";
+import { DataTableDirective } from "angular-datatables";
+import Swal from "sweetalert2";
 
 class DataTablesResponse {
   data: any[];
@@ -17,88 +17,101 @@ class DataTablesResponse {
 }
 
 @Component({
-  selector: 'app-customers',
-  templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.less']
+  selector: "app-customers",
+  templateUrl: "./customers.component.html",
+  styleUrls: ["./customers.component.less"],
 })
 export class CustomersComponent implements OnDestroy, OnInit {
-
   loading = false;
   loadingData = false;
   customers: any;
   listView: boolean = false;
   gridView: boolean = true;
-  image_base_path:any = '';
+  image_base_path: any = "";
   currentUser: User;
-  customerInfo:any = '';
-  products_count:any;
-  dtOptions:any;
-  pagenumber:any = 1;
-  loadmoreflag:boolean = true;
-  customerslists:any;
-
-  @ViewChild(DataTableDirective, {static: false})
+  customerInfo: any = "";
+  products_count: any;
+  dtOptions: any;
+  pagenumber: any = 1;
+  loadmoreflag: boolean = true;
+  customerslists: any;
+  countries = {
+    1: "India",
+    2: "Norway",
+    3: "France",
+    4: "Italy",
+    5: "UK",
+    6: "Sweden",
+    7: "Germany",
+    8: "Poland",
+  };
+  @ViewChild(DataTableDirective, { static: false })
   datatableElement: DataTableDirective;
-  
-  constructor(private userService: UserService, private authenticationService: AuthenticationService, private http: HttpClient, private route: ActivatedRoute) {
-    this.currentUser = this.authenticationService.currentUserValue;
-   }
 
-  ngOnInit() {
-        this.loading = true;
-        this.loadingData = true;
-        $('.dataTables_filter').hide();
-        const that = this;
-        this.dtOptions = {
-          pagingType: 'full_numbers',
-          pageLength: 50,
-          serverSide: true,
-          processing: true,
-          "scrollX": true,
-          "dom": '<"top"lr>rt<"bottom"ip><"clear">',
-          // Configure the buttons
-        //   buttons: [{
-        //     extend: 'pdfHtml5',
-        //     text: 'PDF',
-        //     exportOptions: {
-        //         modifier: {
-        //             page: 'current'
-        //         }
-        //     }
-        // }, 'print'],
-          ajax: (dataTablesParameters: any, callback) => {
-            //console.log(this.projectobj);
-            //dataTablesParameters.search.value = this.projectobj;
-            that.http
-              .post<DataTablesResponse>(
-                `${environment.apiUrl}/customers`,
-                 dataTablesParameters, {}
-              ).subscribe(resp => {
-                that.customers = resp.data;
-                this.loading = false;
-                this.loadingData = false;
-                if(resp.data.length > 0) {
-                  this.image_base_path = resp.data[0].image_base_path;
-                }
-                callback({
-                  recordsTotal: resp.recordsTotal,
-                  recordsFiltered: resp.recordsFiltered,
-                  data: resp.data
-                });
-              });
-          },
-          columns: [{ data: 'DT_RowIndex', orderable:false, searchable:false }, { data: 'customer_name', name : 'customer_name' }, { data: 'projects_count', name : 'projects_count', searchable:false}, { name: 'products_count', data: 'products_count',  searchable:false }, { data: 'postal_area' }]
-        };
-        this.getgridData();
+  constructor(
+    private userService: UserService,
+    private authenticationService: AuthenticationService,
+    private http: HttpClient,
+    private route: ActivatedRoute
+  ) {
+    this.currentUser = this.authenticationService.currentUserValue;
   }
 
-  viewType(type){
-    if(type == 'list'){
+  ngOnInit() {
+    this.loading = true;
+    this.loadingData = true;
+    $(".dataTables_filter").hide();
+    const that = this;
+    this.dtOptions = {
+      pagingType: "full_numbers",
+      pageLength: 50,
+      serverSide: true,
+      processing: true,
+      scrollX: true,
+      dom: '<"top"lr>rt<"bottom"ip><"clear">',
+      ajax: (dataTablesParameters: any, callback) => {
+        that.http
+          .post<DataTablesResponse>(
+            `${environment.apiUrl}/customers`,
+            dataTablesParameters,
+            {}
+          )
+          .subscribe((resp) => {
+            that.customers = resp.data;
+            this.customers.forEach((customer) => {
+              customer.country = this.countries[customer.country];
+            });
+            this.loading = false;
+            this.loadingData = false;
+            if (resp.data.length > 0) {
+              this.image_base_path = resp.data[0].image_base_path;
+            }
+            callback({
+              recordsTotal: resp.recordsTotal,
+              recordsFiltered: resp.recordsFiltered,
+              data: resp.data,
+            });
+          });
+      },
+      columns: [
+        { data: "DT_RowIndex", orderable: false, searchable: false },
+        { data: "customer_name", name: "customer_name" },
+        { data: "projects_count", name: "projects_count", searchable: false },
+        { name: "products_count", data: "products_count", searchable: false },
+        { name: "country", data: "country" },
+        { data: "postal_area" },
+      ],
+    };
+    this.getgridData();
+  }
+
+  viewType(type) {
+    if (type == "list") {
       this.listView = true;
-      this.gridView= false;
+      this.gridView = false;
     } else {
       this.listView = false;
-      this.gridView= true;
+      this.gridView = true;
     }
   }
 
